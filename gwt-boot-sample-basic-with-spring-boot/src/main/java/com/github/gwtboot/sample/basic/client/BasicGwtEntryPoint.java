36ee089b0eef0b14e5f28e4c022a002e994c3ebb
lofidewanto <lofidewanto@gmail.com>
2018-03-21 15:12:59 +0100

    add with Spring Boot

--- gwt-boot-sample-basic-with-spring-boot/src/main/java/com/github/gwtboot/sample/basic/client/BasicGwtEntryPoint.java
+++ gwt-boot-sample-basic-with-spring-boot/src/main/java/com/github/gwtboot/sample/basic/client/BasicGwtEntryPoint.java
@@ -16,15 +16,28 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.github.gwtboot.sample.basic.server;
+package com.github.gwtboot.sample.basic.client;
 
-// import org.springframework.boot.SpringApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.logging.Logger;
 
-// @SpringBootApplication
-public class BasicSpringBootApplication {
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.RootPanel;
 
-	public static void main(String[] args) {
-		// SpringApplication.run(BasicSpringBootApplication.class, args);
+public class BasicGwtEntryPoint implements EntryPoint {
+
+	private static Logger logger = Logger
+			.getLogger(BasicGwtEntryPoint.class.getName());
+
+	@Override
+	public void onModuleLoad() {
+		Button button = new Button("Click me");
+		button.addClickHandler(clickEvent -> {
+			Window.alert("Hello World!");
+			logger.info("Hello World!");
+		});
+
+		RootPanel.get("helloButton").add(button);
 	}
 }
