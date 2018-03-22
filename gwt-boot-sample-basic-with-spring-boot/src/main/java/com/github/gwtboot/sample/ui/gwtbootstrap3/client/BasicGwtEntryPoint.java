4b6539be8ad1fa13ee7c77889b228670505c59b6
lofidewanto <lofidewanto@gmail.com>
2018-03-22 17:01:58 +0100

    add gwtbootstrap3

--- gwt-boot-sample-basic-with-spring-boot/src/main/java/com/github/gwtboot/sample/ui/gwtbootstrap3/client/BasicGwtEntryPoint.java
+++ gwt-boot-sample-basic-with-spring-boot/src/main/java/com/github/gwtboot/sample/ui/gwtbootstrap3/client/BasicGwtEntryPoint.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.github.gwtboot.sample.basic.client;
+package com.github.gwtboot.sample.ui.gwtbootstrap3.client;
 
 import java.util.logging.Logger;
 
---
+++ gwt-boot-sample-ui-gwtbootstrap3/src/main/java/com/github/gwtboot/sample/ui/gwtbootstrap3/client/BasicGwtEntryPoint.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.gwtboot.sample.ui.gwtbootstrap3.client;
+
+import java.util.logging.Logger;
+
+import org.gwtbootstrap3.client.ui.Button;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class BasicGwtEntryPoint implements EntryPoint {
+
+	private static Logger logger = Logger
+			.getLogger(BasicGwtEntryPoint.class.getName());
+
+	@Override
+	public void onModuleLoad() {
+		Button button = new Button("Click me");
+		button.addClickHandler(clickEvent -> {
+			Window.alert("Hello World!");
+			logger.info("Hello World!");
+		});
+
+		RootPanel.get("helloButton").add(button);
+	}
+}
