c929ca956f59823116fc6833ce33970243d8452f
lofidewanto <lofidewanto@gmail.com>
2018-03-23 23:29:53 +0100

    update

--- gwt-boot-sample-basic/src/main/java/com/github/gwtboot/sample/basic/client/BasicGwtEntryPoint.java
+++ gwt-boot-sample-basic/src/main/java/com/github/gwtboot/sample/basic/client/BasicGwtEntryPoint.java
@@ -16,22 +16,28 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.github.gwtboot.sample.ui.gwtbootstrap3.client;
+package com.github.gwtboot.sample.basic.client;
 
 import java.util.logging.Logger;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 
-public class GwtBootstrap3EntryPoint implements EntryPoint {
+public class BasicGwtEntryPoint implements EntryPoint {
 
 	private static Logger logger = Logger
-			.getLogger(GwtBootstrap3EntryPoint.class.getName());
+			.getLogger(BasicGwtEntryPoint.class.getName());
 
 	@Override
 	public void onModuleLoad() {
-		HelloWorldView helloWorldView = new HelloWorldView();
+		Button button = new Button("Click me");
+		button.addClickHandler(clickEvent -> {
+			Window.alert("Hello World!");
+			logger.info("Hello World!");
+		});
 
-		RootPanel.get("mainPanel").add(helloWorldView);
+		RootPanel.get("helloButton").add(button);
 	}
 }
