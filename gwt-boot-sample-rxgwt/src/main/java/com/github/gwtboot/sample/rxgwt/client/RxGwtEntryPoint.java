75910469394c9cde6e147dd89c0b693aaec6320c
lofidewanto <lofidewanto@gmail.com>
2018-05-11 12:42:11 +0200

    update rx-gwt

--- gwt-boot-sample-rxgwt/src/main/java/com/github/gwtboot/sample/rxgwt/client/RxGwtEntryPoint.java
+++ gwt-boot-sample-rxgwt/src/main/java/com/github/gwtboot/sample/rxgwt/client/RxGwtEntryPoint.java
@@ -18,57 +18,53 @@
  */
 package com.github.gwtboot.sample.rxgwt.client;
 
-import static org.jboss.gwt.elemento.core.Elements.button;
-import static org.jboss.gwt.elemento.core.Elements.div;
-import static org.jboss.gwt.elemento.core.Elements.input;
-import static org.jboss.gwt.elemento.core.Elements.table;
-import static org.jboss.gwt.elemento.core.Elements.td;
-import static org.jboss.gwt.elemento.core.Elements.tr;
-import static org.jboss.gwt.elemento.core.EventType.click;
-import static org.jboss.gwt.elemento.core.InputType.number;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.intendia.rxgwt2.elemento.RxElemento;
 import elemental2.dom.HTMLButtonElement;
 import elemental2.dom.HTMLDivElement;
 import elemental2.dom.HTMLInputElement;
 import io.reactivex.Observable;
+import org.jboss.gwt.elemento.core.Elements;
+
 import java.util.function.DoubleUnaryOperator;
 import java.util.logging.Logger;
-import org.jboss.gwt.elemento.core.Elements;
+
+import static org.jboss.gwt.elemento.core.Elements.*;
+import static org.jboss.gwt.elemento.core.EventType.click;
+import static org.jboss.gwt.elemento.core.InputType.number;
 
 public class RxGwtEntryPoint implements EntryPoint {
 
     private static Logger logger = Logger
             .getLogger(RxGwtEntryPoint.class.getName());
 
-    HTMLInputElement addIpt;
-    HTMLButtonElement addBtn;
-    HTMLInputElement subIpt;
-    HTMLButtonElement subBtn;
-    HTMLButtonElement resetBtn;
-    HTMLDivElement result;
+    HTMLInputElement addInputElement;
+    HTMLButtonElement addButtonElement;
+    HTMLInputElement subInputElement;
+    HTMLButtonElement subButtonElement;
+    HTMLButtonElement resetButtonElement;
+    HTMLDivElement resultDivElement;
 
     @Override
     public void onModuleLoad() {
         Elements.body().add(table()
                 .add(tr()
-                        .add(td().add(addIpt = input(number).apply(el -> el.valueAsNumber = 1).get()))
-                        .add(td().add(addBtn = button("add").get())))
+                        .add(td().add(addInputElement = input(number).apply(el -> el.valueAsNumber = 1).get()))
+                        .add(td().add(addButtonElement = button("add").get())))
                 .add(tr()
-                        .add(td().add(subIpt = input(number).apply(el -> el.valueAsNumber = 1).get()))
-                        .add(td().add(subBtn = button("sub").get())))
+                        .add(td().add(subInputElement = input(number).apply(el -> el.valueAsNumber = 1).get()))
+                        .add(td().add(subButtonElement = button("sub").get())))
                 .add(tr()
-                        .add(td().add(result = div().get()))
-                        .add(td().add(resetBtn = button("reset").get()))));
+                        .add(td().add(resultDivElement = div().get()))
+                        .add(td().add(resetButtonElement = button("reset").get()))));
 
         Observable<DoubleUnaryOperator> action$ = Observable.merge(
-                RxElemento.fromEvent(addBtn, click).map(ev -> addIpt.valueAsNumber).map(val -> n1 -> n1 + val),
-                RxElemento.fromEvent(subBtn, click).map(ev -> subIpt.valueAsNumber).map(val -> n1 -> n1 - val),
-                RxElemento.fromEvent(resetBtn, click).map(ev -> n1 -> 0));
+                RxElemento.fromEvent(addButtonElement, click).map(ev -> addInputElement.valueAsNumber).map(val -> n1 -> n1 + val),
+                RxElemento.fromEvent(subButtonElement, click).map(ev -> subInputElement.valueAsNumber).map(val -> n1 -> n1 - val),
+                RxElemento.fromEvent(resetButtonElement, click).map(ev -> n1 -> 0));
 
         action$.scan(0., (acc, n) -> n.applyAsDouble(acc))
                 .doOnNext(n -> logger.info("value change: " + n))
-                .subscribe(n -> result.textContent = Double.toString(n));
+                .subscribe(n -> resultDivElement.textContent = Double.toString(n));
     }
 }
