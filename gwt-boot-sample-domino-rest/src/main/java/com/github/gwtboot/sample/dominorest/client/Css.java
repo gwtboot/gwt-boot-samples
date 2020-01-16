b96f96053a4f49a819bb51780842e3b8ea2bf1a3
lofidewanto <lofidewanto@gmail.com>
2020-01-16 22:39:57 +0100

    add domino-rest example

---
+++ gwt-boot-sample-domino-rest/src/main/java/com/github/gwtboot/sample/dominorest/client/Css.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.github.gwtboot.sample.dominorest.client;
+
+/**
+ * Contains Css classes used in the application. The constants in this interface are not involved in
+ * any kind of code generation or GWT magic. They're just here to have them in one place.
+ */
+public interface Css {
+
+    String backgroundSecondary = "background-secondary";
+    String backgroundSuccess = "background-success";
+    String border = "border";
+    String card = "card";
+    String cardBody = "card-body";
+    String center = "center";
+    String container = "container";
+    String flexCenter = "flex-center";
+    String flexTop = "flex-top";
+    String marginRightLarge = "margin-right-large";
+    String marginTopLarge = "margin-top-large";
+    String paddingLeftLarge = "padding-left-large";
+    String paddingRightLarge = "padding-right-large";
+    String row = "row";
+    String shadow = "shadow";
+}
